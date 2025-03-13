1248cd0b249a38870a30bda728990118ff820707
Sergey Ilyev <ilev.sv@phystech.edu>
2025-03-13 15:10:38 +0300

    Refactor voltage tests

--- tests/test_pulse.py
+++ tests/test_pulse.py
@@ -1,31 +1,19 @@
+import pytest
+
 from waveform_generator import Pulse
 
 
-def test_max_voltage_positive():
-    pulse = Pulse(amplitude=5.0, duration=10.0, dc_bias=2.0)
-    assert pulse.max_voltage == 7.0
-
-
-def test_max_voltage_negative():
-    pulse = Pulse(amplitude=-5.0, duration=10.0, dc_bias=-2.0)
-    assert pulse.max_voltage == 7.0
-
-
-def test_max_voltage_different_signs_1():
-    pulse = Pulse(amplitude=2.0, duration=10.0, dc_bias=-5.0)
-    assert pulse.max_voltage == 5.0
-
-
-def test_max_voltage_different_signs_2():
-    pulse = Pulse(amplitude=6.0, duration=10.0, dc_bias=-5.0)
-    assert pulse.max_voltage == 5.0
-
-
-def test_max_voltage_different_signs_3():
-    pulse = Pulse(amplitude=-5.0, duration=10.0, dc_bias=6.0)
-    assert pulse.max_voltage == 6.0
-
-
-def test_max_voltage_different_signs_4():
-    pulse = Pulse(amplitude=-5.0, duration=10.0, dc_bias=2.0)
-    assert pulse.max_voltage == 3.0
+@pytest.mark.parametrize(
+    "amplitude, dc_bias, expected_max",
+    [
+        (5.0, 2.0, 7.0),
+        (-5.0, -2.0, 7.0),
+        (2.0, -5.0, 5.0),
+        (6.0, -5.0, 5.0),
+        (-5.0, 6.0, 6.0),
+        (-5.0, 2.0, 3.0),
+    ],
+)
+def test_max_voltage(amplitude, dc_bias, expected_max):
+    pulse = Pulse(amplitude=amplitude, duration=1.0, dc_bias=dc_bias)
+    assert pulse.max_voltage == expected_max
