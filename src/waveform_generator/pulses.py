51ddef7dfca6818bc710e8ff70776507fcf021bc
Sergey Ilyev <ilev.sv@phystech.edu>
2025-06-01 20:48:15 +0300

    Fix to_vectors when delay = 0

--- src/waveform_generator/pulses.py
+++ src/waveform_generator/pulses.py
@@ -87,9 +87,11 @@ class TrapezoidalPulse(Pulse):
 
     def to_vectors(self):
         """Return the trapezoidal pulse as a list of vectors."""
-        return [
-            [self.delay, self.dc_bias],
+        vectors = [
             [self.rise_time, self.dc_bias + self.amplitude],
             [self.pulse_width, self.dc_bias + self.amplitude],
             [self.fall_time, self.dc_bias],
         ]
+        if self.delay:
+            vectors = [[self.delay, self.dc_bias]] + vectors
+        return vectors
